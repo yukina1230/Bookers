fc5399933b820a6d196ae2f30c46e1c2ed84b013
EC2 Default User <ec2-user@ip-172-31-17-202.ec2.internal>
2021-03-04 06:57:59 +0000

    [ADD]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
     end
   end
 
@@ -21,10 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:value] = 'Book was successfully updated.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
     end
   end
 
