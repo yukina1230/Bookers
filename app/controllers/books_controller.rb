e94cb94901c7cc10dcce654fc0b787bf509badd6
EC2 Default User <ec2-user@ip-172-31-17-202.ec2.internal>
2021-03-04 04:37:43 +0000

    [Add]flash_message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,10 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    end
   end
 
   def show
@@ -19,15 +21,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:value] = 'Book was successfully updated.'
+      redirect_to book_path(book.id)
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:alert] = 'Book was successfully destroyed.'
+      redirect_to books_path
+    end
   end
 
   private
